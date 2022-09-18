27d000e8fa5ef9c83d22f5464a991ed6333d17a4
hdgaadd <53108370+hdgaadd@users.noreply.github.com>
2022-09-18 17:41:33 +0800

    upgrade the App named excel

--- src/main/java/org/codeman/core/personal/excel/App.java
+++ src/main/java/org/codeman/core/personal/excel/App.java
@@ -20,11 +20,11 @@ import java.util.List;
  * @Description: excel转换为String，打印出包含关键字符串的行
  */
 public class App {
-    private final static String PATH = Address.nameAndAddress("excel.xlsx", App.class);
+    private final static String PATH = Address.nameAndAddress("test.xlsx", App.class);
 
-    private final static String KEYWORD1 = "外企";
+    private final static String KEYWORD1 = "A";
 
-    private final static String KEYWORD2 = "深圳";
+    private final static String KEYWORD2 = "B";
 
     public static void main(String[] args) throws IOException {
         Sheet sheet = new XSSFWorkbook(new FileInputStream(new File(PATH))).getSheetAt(0);
@@ -36,8 +36,8 @@ public class App {
             for (Cell cell : row) {
                 builder.append(cell.toString()).append("  ");
             }
-            // 是否包含关键字
-            if (builder.toString().contains(KEYWORD1) && builder.toString().contains(KEYWORD2) || index++ == 0) {
+            // 是否包含关键字 || 打印表头
+            if (index++ == 0 || builder.toString().contains(KEYWORD1) && builder.toString().contains(KEYWORD2)) {
                 needLine.add(builder.toString());
             }
         }
